4589f5f19520ce039f3479bcadaa0c0922850f12
weslleylmurdock <weslleyluiz@outlook.com.br>
2022-10-08 10:37:50 -0300

    - feat: tests implementation

---
+++ RCPad.UITests/AppInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace RCPad.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.StartApp();
+            }
+            
+            // Workaround for iOS simulator reset bug
+            Environment.SetEnvironmentVariable("UITEST_FORCE_IOS_SIM_RESTART", "1");
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ RCPad.UITests/Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace RCPad.UITests
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Text("Pad A"));
+            app.Screenshot("Controls");
+
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
